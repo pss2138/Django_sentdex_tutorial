6e12c229581b5b430850a224b39ecc72232f4052
pss2138 <pss2138@gmail.com>
2020-08-26 08:47:46 +0900

    register function added

--- main/urls.py
+++ main/urls.py
@@ -19,5 +19,6 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
-    path("", views.homepage, name="homepage")
+    path("", views.homepage, name="homepage"),
+    path("register/", views.register, name="register"),
 ]
