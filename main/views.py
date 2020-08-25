01bdc21927e72a2996cc544ef1579a58f8f4ce92
pss2138 <pss2138@gmail.com>
2020-08-25 12:37:56 +0900

    Views and Templates for the homepage and admin

--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tutorial
 
 def homepage(request):
-    return HttpResponse("Wow this is an <strong>awesome</strong> tutorial")
+    return render(request=request,
+                  template_name="main/home.html",
+                  context={"tutorials": Tutorial.objects.all})
